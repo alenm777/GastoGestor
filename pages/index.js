a6ef61b08befd0dfb3f90f84b02016dc0de8ae2a
alenm777 <alenm.1717@gmail.com>
2023-10-17 19:54:30 -0300

    continuamois

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
+import { IoStatsChart } from 'react-icons/io5'
 
 export default function Home() {
   return (
-  <header>
+  <header className="flex items-center justify-between">
 
     {/*Informacion de Usuario */}
-    <div>
+    <div className="flex items-center gap-2">
       { /* Imagenes */}
       <div className="h-[40px] w-[40px] rounded-full overflow-hidden">
       <img 
@@ -16,6 +17,14 @@ export default function Home() {
       {/* Nombre */}
       <small>Hola, Lionel!</small>
     </div>
+
+{ /* derecha de la barra de navegacion  */}
+    <nav className="flex items-center gap-2">
+      <div>
+        <IoStatsChart className='text-2xl' />
+        </div>
+      <div><button className='px-4 py-2 text-sm capitalize rounded-xl bg-red-600 border-red-600 text-white'> Cerrar sesión</button></div>
+    </nav>
   </header>
   )
 }
